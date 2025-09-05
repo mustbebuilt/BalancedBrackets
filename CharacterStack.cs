b93c04c685d560a8d8c82a881f2ab5f8f575feef
Jaya Lakshmi Tangirala <jt3951@hallam.shu.ac.uk>
2025-09-05 16:16:05 +0100

    Add project files.

---
+++ CharacterStack.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BalancedBrackets
+{
+    internal class CharacterStack
+    {
+        private char[] stack; //Array of elements
+        private int size;    //maximum number of elements in stack
+        private int top;     //index of top element in the stack.
+
+        public CharacterStack(int n)  //n is the maximum size of stack.
+        {
+            size = n;
+            stack = new char[size]; //allocate space to array of size n
+            top = -1;              // No elements in the stack initially.
+        }
+       public bool isEmpty()
+        {
+            if (top == -1)
+                return true;
+            else
+                return false;
+        }
+        public bool isFull()
+        {
+            if (top==size-1)
+                return true;
+            else 
+                return false;
+        }
+        public void push(char element)
+        {
+            if (isFull())
+                Console.WriteLine("Stack OverFlow!");
+            else
+            {
+                top++;
+                stack[top] = element;
+            }
+        }
+        public char pop() 
+        { 
+            if(isEmpty())
+            {
+                Console.WriteLine("Stack Underflow!!");
+                return default(char);
+            }
+            else
+            {
+                char element = stack[top];
+                top--;
+                return element;
+            }   
+        }
+        public char peek()
+        {
+            if (isEmpty())
+            {
+                Console.WriteLine("Stack Empty");
+                return '\0';
+            }                
+            else
+                return stack[top];
+        }
+        public void print()
+        {
+            if (isEmpty())
+                Console.WriteLine("Stack is empty");
+            else
+            {
+                Console.WriteLine("The elements of Stack are: ");
+                for (int i = top; i >= 0; i--)                
+                    Console.WriteLine(stack[i]);                
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿namespace BalancedBrackets
+{
+    internal class Program
+    {
+        static bool areBracketsBalanced(string expr)
+        {
+            CharacterStack s=new CharacterStack(expr.Length);
+
+            foreach (char c in expr)
+            {
+                if (c == '(' || c == '{' || c == '[')                
+                    s.push(c); // push opening brackets                
+                else if (c == ')' || c == '}' || c == ']')
+                {
+                    if (s.isEmpty())
+                        return false; // closing bracket with no matching opening
+
+                    char top = s.pop();
+
+                    // check if matching pair
+                    if ((c == ')' && top != '(') || (c == '}' && top != '{') || (c == ']' && top != '['))                    
+                        return false;                    
+                }
+            }
+
+            if(s.isEmpty())
+                return true; // all brackets matched
+            else 
+                return false;
+        }
+
+        private static void Main(string[] args)
+        {
+            Console.Write("Enter an expression: ");
+            string expression = Console.ReadLine();
+
+            if (areBracketsBalanced(expression))
+                Console.WriteLine("Brackets are balanced.");
+            else
+                Console.WriteLine("Brackets are not balanced.");
+        }                
+    }
+}
